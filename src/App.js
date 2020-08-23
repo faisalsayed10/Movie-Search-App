6ea63c50459f8cf4308c0b1e44798450e522f3c0
fayd <36627266+faisalsayed10@users.noreply.github.com>
2020-08-23 13:02:49 +0530

    Added TMDB Logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.css";
 import SearchMovies from "./SearchMovies";
+
 const appTitle = '🍿Binger'
+const imgStyle = {
+  'width': '40%',
+  'margin': '0 auto'
+}
+
+
 export default class App extends React.Component {
   render() {
     return (
     <div className="container">
+      <img style={imgStyle} src='https://bit.ly/3aPx2gv' alt='TMDB LOGO'></img>
       <h1 className='title'>{appTitle}</h1>
       <SearchMovies />
     </div>
