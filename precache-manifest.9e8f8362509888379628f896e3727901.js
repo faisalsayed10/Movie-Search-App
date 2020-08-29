817fc61b40f552d815acf66a7d4fc65df7186cf7
Afzal Sayed <afzalsayed96@gmail.com>
2020-08-29 15:17:46 +0530

    Updates

---
+++ precache-manifest.9e8f8362509888379628f896e3727901.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10751522d52a7abac0c2d0534af7cc3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd79e09e8740141128c4",
+    "url": "/static/css/main.2753e5f5.chunk.css"
+  },
+  {
+    "revision": "c0ca98c27cd225a96aa3",
+    "url": "/static/js/2.6cd26fe2.chunk.js"
+  },
+  {
+    "revision": "bd79e09e8740141128c4",
+    "url": "/static/js/main.da8d2412.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
